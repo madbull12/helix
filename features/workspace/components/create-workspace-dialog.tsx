8b23e8ed266649de176837d565752a0d5d8c79b8
madbull12 <huangandrian02@gmail.com>
2026-01-12 23:31:02 +0700

    create workspace dialog component

--- features/workspace/components/create-workspace-dialog.tsx
+++ features/workspace/components/create-workspace-dialog.tsx
@@ -1,3 +1,5 @@
+"use client"
+
 import { Button, buttonVariants } from "@/components/ui/button";
 import {
   Credenza,
@@ -11,30 +13,96 @@ import {
   CredenzaTrigger,
 } from "@/components/ui/credenza";
 import { Plus } from "lucide-react";
-
+import { useForm } from "@tanstack/react-form";
+import {
+  Field,
+  FieldDescription,
+  FieldError,
+  FieldGroup,
+  FieldLabel,
+} from "@/components/ui/field";
+import { Input } from "@/components/ui/input";
+import * as z from "zod";
+const createWorkspaceSchema = z.object({
+  name: z.string().min(1, {
+    error: "Name shouldn't be empty",
+  }),
+});
 const CreateWorkspaceDialog = () => {
+  const form = useForm({
+    defaultValues: {
+      name: "",
+    },
+    validators: {
+      onSubmit: createWorkspaceSchema,
+    },
+    onSubmit: async ({ value }) => {
+      console.log(value);
+    },
+  });
   return (
     <Credenza>
       <CredenzaTrigger asChild>
-        <Button variant={"outline"} size={'sm'} className="cursor-pointer py-5      border-dashed self-center">
+        <Button
+          variant={"outline"}
+          size={"sm"}
+          className="cursor-pointer py-5      border-dashed self-center"
+        >
           <Plus className="size-4" />
         </Button>
       </CredenzaTrigger>
       <CredenzaContent>
         <CredenzaHeader>
-          <CredenzaTitle>Credenza</CredenzaTitle>
+          <CredenzaTitle>Create Workspace</CredenzaTitle>
           <CredenzaDescription>
-            A responsive modal component for shadcn/ui.
+            Create a new workspace to get started
           </CredenzaDescription>
         </CredenzaHeader>
+
         <CredenzaBody>
-          This component is built using shadcn/ui&apos;s dialog and drawer
-          component, which is built on top of Vaul.
+          <form
+            id="create-workspace-form"
+            onSubmit={(e) => {
+              e.preventDefault();
+              form.handleSubmit();
+            }}
+          >
+            <FieldGroup>
+              <form.Field
+                name="name"
+                children={(field) => {
+                  const isInvalid =
+                    field.state.meta.isTouched && !field.state.meta.isValid;
+                  return (
+                    <Field data-invalid={isInvalid}>
+                      <FieldLabel htmlFor={field.name}>
+                        Name
+                      </FieldLabel>
+                      <Input
+                        id={field.name}
+                        name={field.name}
+                        value={field.state.value}
+                        onBlur={field.handleBlur}
+                        onChange={(e) => field.handleChange(e.target.value)}
+                        aria-invalid={isInvalid}
+                        placeholder="My workspace..."
+                        autoComplete="off"
+                      />
+                      {isInvalid && (
+                        <FieldError errors={field.state.meta.errors} />
+                      )}
+                    </Field>
+                  );
+                }}
+              />
+            </FieldGroup>
+          </form>
         </CredenzaBody>
+
         <CredenzaFooter>
-          <CredenzaClose asChild>
-            <Button>Close</Button>
-          </CredenzaClose>
+          <Button type="submit" form="create-workspace-form">
+            Create
+          </Button>
         </CredenzaFooter>
       </CredenzaContent>
     </Credenza>
